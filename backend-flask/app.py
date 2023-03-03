7b48f63f9342f4a2dca94065086adbe793604fb4
Ify Eleweke <ify.eleweke@weaveup.com>
2023-03-03 18:48:35 +0000

    Integrated rollbar for error logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,11 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # Configuring Logger to Use CloudWatch
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
@@ -73,6 +78,30 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.after_request
 def after_request(response):
