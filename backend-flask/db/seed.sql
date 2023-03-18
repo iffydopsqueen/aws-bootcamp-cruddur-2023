fa429ba1f1d19b5fd012e2cb81c3221eb27ee059
Ify Eleweke <omaleweks@gmail.com>
2023-03-18 21:50:17 +0000

    Added a script for seeding data and some mock data

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,15 @@
+-- insert mock data into our tables
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('Gentle Warrior', 'dopsqueen' ,'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'MOCK'),
+  ('Zeus Sucker', 'godofthunder' ,'MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'dopsqueen' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
