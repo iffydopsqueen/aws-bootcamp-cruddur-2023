05c10a32312b9e5bb19f55ffbff41ab788321cd0
Ify Eleweke <omaleweks@gmail.com>
2023-03-23 17:51:26 +0000

    Setting up postgres driver

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -21,7 +21,20 @@ class HomeActivities:
       span.set_attribute("app.now", now.isoformat())
 
       sql = query_wrap_array("""
-      SELECT * FROM public.activities
+      SELECT 
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC      
       """)
       print(sql)
 
@@ -31,7 +44,5 @@ class HomeActivities:
           # this will return a tuple
           # the first field being the data
           json = cur.fetchone()
-      print("45-----------")
       return json[0]
-
       return results
