baa9c0e07af2933705ef79e31a412d7873c5e095
Ify Eleweke <omaleweks@gmail.com>
2023-04-06 22:04:12 +0000

    Implemented pattern A & B of listing messages in a message group

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -8,7 +8,6 @@ import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
 import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
-import MessageGroupNewPage from './pages/MessageGroupNewPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import {
@@ -52,7 +51,7 @@ const router = createBrowserRouter([
     element: <MessageGroupsPage />
   },
   {
-    path: "/messages/@:handle",
+    path: "/messages/:message_group_uuid",
     element: <MessageGroupPage />
   },
   {
