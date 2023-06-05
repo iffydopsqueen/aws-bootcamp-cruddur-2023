3e7390c987155d662450ca568816460342f9efec
Ify Eleweke <omaleweks@gmail.com>
2023-06-05 02:17:12 +0000

    Configured CORS to only permit traffic from my domain

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -58,8 +58,8 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 # AWS X-Ray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 app = Flask(__name__)
 
