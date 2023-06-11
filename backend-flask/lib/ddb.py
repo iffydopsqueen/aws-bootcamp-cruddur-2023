c3e991c8d8cd45335302a5cdeb10f485fc990541
Ify Eleweke <omaleweks@gmail.com>
2023-06-11 01:22:17 +0000

    Implemented timezone fixes

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -78,8 +78,7 @@ class Ddb:
 
   # create message in an existing msg grp
   def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
-    now = datetime.now(timezone.utc).astimezone().isoformat()
-    created_at = now
+    created_at = datetime.now().isoformat()
     message_uuid = str(uuid.uuid4())
 
     record = {
